36ac0ce19a306a177c6a0352d96d867dd598b4f0
MindStudioOfficial <marc@bachrc.de>
2025-02-03 12:57:37 +0100

    ✨ Refactor NDISend class to support multiple instances and improve frame handling; Add second off screen widget send

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-import 'dart:ffi';
+import 'dart:ffi' as ffi;
+import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 import 'package:ffi/ffi.dart';
@@ -9,12 +10,8 @@ import 'package:ndisend/ndi_ffi_bindings.dart';
 
 import 'package:ndisend/ndisend.dart';
 
-late NDISend ndiSend;
-
 void main() {
-  ndiSend = NDISend();
   runApp(const Main());
-  ndiSend.stopSendFrames();
 }
 
 class Main extends StatefulWidget {
@@ -35,7 +32,7 @@ class _MainState extends State<Main> {
   late NDIFrame frame;
 
   // where the pixels are stored
-  late Pointer<Uint8> pData;
+  late ffi.Pointer<ffi.Uint8> pData;
 
   // the size of the pixel buffer in bytes
   int maxLen = 1920 * 1080 * 4;
@@ -59,11 +56,19 @@ class _MainState extends State<Main> {
         );
   }
 
+  late NDISend ndiSend;
+  late NDISend ndiSend2;
+
+  late NDIFrame offScreenFrame;
+  late ffi.Pointer<ffi.Uint8> offScreenData;
+
   @override
   void initState() {
     super.initState();
+    ndiSend = NDISend('Flutter NDI SEND');
+
     // allocate space for the pixel buffer
-    pData = calloc.call<Uint8>(maxLen);
+    pData = malloc.call<ffi.Uint8>(maxLen);
     // create a new frame internally creating the NDIlib struct
     frame = NDIFrame(
       width: 1920,
@@ -81,11 +86,27 @@ class _MainState extends State<Main> {
     Future.delayed(const Duration(milliseconds: 10), () {
       timer = Timer.periodic(
           Duration(
-            milliseconds: 500 ~/ (frame.frameRateN / frame.frameRateD),
+            milliseconds: 1000 ~/ (frame.frameRateN / frame.frameRateD),
           ), (t) {
         update();
       });
     });
+
+    ndiSend2 = NDISend('Flutter NDI SEND OffScreen');
+
+    offScreenData = malloc.call<ffi.Uint8>(maxLen);
+
+    offScreenFrame = NDIFrame(
+      width: 1920,
+      height: 1080,
+      fourCC: NDIlib_FourCC_video_type_e.NDIlib_FourCC_type_RGBA,
+      pDataA: offScreenData.address,
+      format: NDIlib_frame_format_type_e.NDIlib_frame_format_type_progressive,
+      bytesPerPixel: 4,
+      frameRateN: 30000,
+    );
+
+    ndiSend2.sendFrames(offScreenFrame);
   }
 
   @override
@@ -94,7 +115,9 @@ class _MainState extends State<Main> {
     timer?.cancel();
     ndiSend.stopSendFrames();
     frame.destroy();
-    //calloc.free(pData);
+
+    ndiSend2.stopSendFrames();
+    offScreenFrame.destroy();
   }
 
   // restart the sending process on hot reload
@@ -102,17 +125,23 @@ class _MainState extends State<Main> {
   void reassemble() {
     super.reassemble();
     timer?.cancel();
+
     ndiSend
       ..stopSendFrames()
       ..sendFrames(frame);
+
     Future.delayed(const Duration(milliseconds: 10), () {
       timer = Timer.periodic(
           Duration(
-            milliseconds: 500 ~/ (frame.frameRateN / frame.frameRateD),
+            milliseconds: 1000 ~/ (frame.frameRateN / frame.frameRateD),
           ), (t) {
         update();
       });
     });
+
+    ndiSend2
+      ..stopSendFrames()
+      ..sendFrames(offScreenFrame);
   }
 
   void update() {
@@ -186,7 +215,104 @@ class _MainState extends State<Main> {
             );
           },
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final sw = Stopwatch()..start();
+            final data = await createImageFromWidget(
+              const SizedBox(
+                width: 1920,
+                height: 1080,
+                child: Placeholder(),
+              ),
+              context,
+              logicalSize: const Size(1920, 1080),
+              imageSize: const Size(1920, 1080),
+            );
+
+            if (data == null) {
+              print('Failed to create image from widget');
+              return;
+            }
+
+            final buf = data.asUint8List();
+
+            offScreenData.asTypedList(maxLen).setRange(0, buf.length, buf);
+
+            ndiSend2.updateFrame(offScreenFrame);
+            sw.stop();
+            print(
+              'Took ${sw.elapsedMilliseconds}ms to send image from widget',
+            );
+          },
+          child: const Icon(Icons.camera),
+        ),
       ),
     );
   }
+}
+
+Future<ByteBuffer?> createImageFromWidget(
+  Widget widget,
+  BuildContext context, {
+  Duration? wait,
+  Size? logicalSize,
+  Size? imageSize,
+}) async {
+  final sw = Stopwatch()..start();
+  final repaintBoundary = RenderRepaintBoundary();
+
+  logicalSize ??=
+      View.of(context).physicalSize / View.of(context).devicePixelRatio;
+
+  imageSize ??= View.of(context).physicalSize;
+
+  assert(
+    logicalSize.aspectRatio == imageSize.aspectRatio,
+    'The logical size and the image size must have the same aspect ratio.',
+  );
+
+  final renderView = RenderView(
+    child: RenderPositionedBox(
+      child: repaintBoundary,
+    ),
+    configuration: ViewConfiguration(
+      logicalConstraints: BoxConstraints.tight(logicalSize),
+    ),
+    view: View.of(context),
+  );
+
+  final pipelineOwner = PipelineOwner();
+  final buildOwner = BuildOwner(focusManager: FocusManager());
+
+  pipelineOwner.rootNode = renderView;
+  renderView.prepareInitialFrame();
+
+  final rootElement = RenderObjectToWidgetAdapter<RenderBox>(
+    container: repaintBoundary,
+    child: Directionality(textDirection: TextDirection.ltr, child: widget),
+  ).attachToRenderTree(buildOwner);
+
+  buildOwner.buildScope(rootElement);
+
+  if (wait != null) {
+    await Future<void>.delayed(wait);
+  }
+
+  buildOwner
+    ..buildScope(rootElement)
+    ..finalizeTree();
+
+  pipelineOwner
+    ..flushLayout()
+    ..flushCompositingBits()
+    ..flushPaint();
+
+  final image = await repaintBoundary.toImage(
+    pixelRatio: imageSize.width / logicalSize.width,
+  );
+
+  final byteData = await image.toByteData();
+  sw.stop();
+  print('Took ${sw.elapsedMilliseconds}ms to create image from widget');
+  return byteData?.buffer;
 }
--- lib/ndisend.dart
+++ lib/ndisend.dart
@@ -8,34 +8,57 @@ LibNDIFFI _ndi =
     LibNDIFFI(DynamicLibrary.open('bin/Processing.NDI.Lib.x64.dll'));
 
 class NDISend {
-  NDISend() {
+  NDISend(this.name) {
     _ndi.NDIlib_v5_load();
     if (!_ndi.NDIlib_initialize()) {
       throw Exception('Could not initialize NDI');
     }
   }
 
+  final String name;
+
   ReceivePort? _sendReceivePort;
   Isolate? _sendIsolate;
   SendPort? _sendSendPort;
 
-  NDIlib_send_instance_t? _pSendInstance;
-
-  Pointer? _pSendName;
+  NDIlib_send_instance_t? _pSend;
 
   Future<void> sendFrames(NDIFrame frame) async {
     final completer = Completer<void>();
     _sendReceivePort = ReceivePort();
+
+    _sendIsolate = await Isolate.spawn(
+      _sendFrames,
+      SendObject(
+        frame: frame,
+        sendPort: _sendReceivePort!.sendPort,
+        sendName: name,
+      ),
+      paused: true,
+      debugName: 'NDISend Isolate $name',
+    );
+
+    _sendIsolate?.addOnExitListener(
+      _sendReceivePort!.sendPort,
+      response: 'onExit',
+    );
+
+    _sendIsolate?.addErrorListener(_sendReceivePort!.sendPort);
+
     _sendReceivePort!.listen(
       (data) {
-        if (data is SendPort) {
-          _sendSendPort = data;
-        }
-        if (data is Map<String, int>) {
-          if (data['pSendInstance'] != null && data['pSendName'] != null) {
-            _pSendInstance = Pointer.fromAddress(data['pSendInstance']!);
-            _pSendName = Pointer.fromAddress(data['pSendName']!);
-          }
+        switch (data) {
+          case 'onExit':
+            _killSendIsolate();
+          case 'error':
+            print('error: $data');
+          case final SendPort sendPort:
+            _sendSendPort = sendPort;
+          case {
+              'pSendInstance': final int aSendInstance,
+              'pSendName': _,
+            }:
+            _pSend = Pointer.fromAddress(aSendInstance).cast();
         }
       },
       onDone: () {
@@ -44,28 +67,47 @@ class NDISend {
       },
     );
 
-    _sendIsolate = await Isolate.spawn(
-      _sendFrames,
-      SendObject(frame: frame, sendPort: _sendReceivePort!.sendPort),
-    );
+    _sendIsolate?.resume(_sendIsolate!.pauseCapability!);
+
+    print('started sending');
+
     return completer.future;
   }
 
-  void stopSendFrames() {
+  Future<void> stopSendFrames() async {
     print('stopping');
-    if (_sendIsolate != null && _sendReceivePort != null) {
-      _sendReceivePort!.close();
-      _sendIsolate!.kill(priority: Isolate.immediate);
-      _sendIsolate = null;
-      _sendReceivePort = null;
-    }
-    if (_pSendInstance != null) {
-      _ndi.NDIlib_send_destroy(_pSendInstance!);
-      print('destroying send instance');
-    }
 
-    if (_pSendName != null) ffi.calloc.free(_pSendName!);
-    print('stopped');
+    _sendSendPort?.send('end');
+
+    if (_sendIsolate != null) {
+      var counter = 0;
+
+      await Future.doWhile(() async {
+        print('waiting for isolate to exit');
+
+        if (counter++ > 20) {
+          _killSendIsolate();
+          print('killed unresponsive isolate');
+          return false;
+        }
+
+        return Future<bool>.delayed(const Duration(milliseconds: 250), () {
+          return _sendIsolate != null;
+        });
+      });
+    } else {
+      _killSendIsolate();
+    }
+  }
+
+  void _killSendIsolate() {
+    _sendReceivePort?.close();
+    _sendIsolate?.kill(priority: Isolate.immediate);
+    _sendIsolate = null;
+    _sendReceivePort = null;
+    _sendSendPort = null;
+
+    if (_pSend != null) _ndi.NDIlib_send_destroy(_pSend!);
   }
 
   void updateFrame(NDIFrame frame) {
@@ -75,23 +117,27 @@ class NDISend {
   }
 
   static Future<void> _sendFrames(SendObject object) async {
+    var end = false;
+
     final receivePort = ReceivePort();
     object.sendPort.send(receivePort.sendPort);
 
     receivePort.listen((data) {
-      if (data is NDIFrame) {
-        //print("gotFrame");
-        if (!object.frame.destroyed &&
-            data.pFrame.address != object.frame.pFrame.address) {
-          object.frame.destroy();
-          object.frame = data;
-        }
+      switch (data) {
+        case final NDIFrame frame:
+          if (!object.frame.destroyed &&
+              frame.pFrame.address != object.frame.pFrame.address) {
+            object.frame.destroy();
+            object.frame = frame;
+          }
+        case 'end':
+          end = true;
       }
     });
 
-    final pCreateSettings = ffi.calloc.call<NDIlib_send_create_t>();
+    final pCreateSettings = ffi.malloc.call<NDIlib_send_create_t>();
 
-    final pNDIName = 'NDISend by MindStudio'.toNativeUtf8().cast<Char>();
+    final pNDIName = object.sendName.toNativeUtf8().cast<Char>();
     pCreateSettings.ref.p_ndi_name = pNDIName;
     pCreateSettings.ref.clock_audio = false;
     pCreateSettings.ref.clock_video = false;
@@ -107,7 +153,7 @@ class NDISend {
       'pSendName': pNDIName.address,
     });
 
-    while (true) {
+    while (!end) {
       _ndi.NDIlib_send_send_video_async_v2(pSend, object.frame.pFrame);
       await Future<void>.delayed(
         Duration(
@@ -116,14 +162,26 @@ class NDISend {
         ),
       );
     }
+
+    _ndi.NDIlib_send_destroy(pSend);
+
+    ffi.malloc.free(pCreateSettings);
+    ffi.malloc.free(pNDIName);
+
+    Isolate.exit();
   }
 }
 
 class SendObject {
-  SendObject({required this.frame, required this.sendPort});
+  SendObject({
+    required this.frame,
+    required this.sendPort,
+    required this.sendName,
+  });
 
   NDIFrame frame;
   SendPort sendPort;
+  final String sendName;
 }
 
 class NDIFrame {
@@ -169,7 +227,7 @@ class NDIFrame {
 
   void destroy() {
     _destroyed = true;
-    ffi.calloc.free(Pointer.fromAddress(pDataA));
-    ffi.calloc.free(Pointer.fromAddress(_pFrameA));
+    ffi.malloc.free(Pointer.fromAddress(pDataA));
+    ffi.malloc.free(Pointer.fromAddress(_pFrameA));
   }
 }
